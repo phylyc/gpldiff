fd63e7f898221c35bbde3cd5de0e92e44d203e1a
David J. H. Shih <djh.shih@gmail.com>
2018-09-20 15:56:38 -0500

    Moved FDR code to stats.

--- R/fdr.R
+++
@@ -1,9 +0,0 @@
-#' Compute the Bayesian false discovery rate from posterior probability.
-#'
-#' @param v  sorted (by descending order) posterior probablity that a gene 
-#'           is in the positive class
-#' @return false discovery rate incurred by declaring each gene as significant 
-#'         in order
-bayesian_fdr <- function(v) {
-	cumsum(1 - v) / 1:length(v)
-}
--- R/stats.R
+++ R/stats.R
@@ -44,3 +44,13 @@ autocorl1 <- function(x) {
 	cor(x[-length(x)],x[-1])
 }
 
+#' Compute the Bayesian false discovery rate from posterior probability.
+#'
+#' @param v  sorted (by descending order) posterior probablity that a gene 
+#'           is in the positive class
+#' @return false discovery rate incurred by declaring each gene as significant 
+#'         in order
+bayesian_fdr <- function(v) {
+	cumsum(1 - v) / 1:length(v)
+}
+
